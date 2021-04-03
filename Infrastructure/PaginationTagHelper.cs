0e8b1a7fd38164ad52321834481cd68d8b147fc5
bsperry <69691639+bsperry@users.noreply.github.com>
2021-04-02 20:09:14 -0600

    Add project files.

---
+++ Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,54 @@
+﻿
+
+using Assignment10.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Collections.Generic;
+
+namespace Assignment10.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-info")]
+    public class PaginationTagHelper: TagHelper
+    {
+        private IUrlHelperFactory urlInfo;
+
+        public PaginationTagHelper (IUrlHelperFactory uhf)
+        {
+            urlInfo = uhf;
+        }
+
+        public PageNumberingInfo PageInfo { get; set; }
+
+        //dictionary of the key value pairs we are creating for teamid and team name for filtering on teams
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string, object> KeyValuePairs { get; set; } = new Dictionary<string, object>();
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelp = urlInfo.GetUrlHelper(ViewContext);
+            
+            TagBuilder finishedTag = new TagBuilder("div");
+
+            for (int i = 1; i <= PageInfo.NumPages; i++)
+            {
+                TagBuilder individualTag = new TagBuilder("a");
+
+                KeyValuePairs["pageNum"] = i;
+                individualTag.Attributes["href"] = urlHelp.Action("Index", KeyValuePairs);
+                individualTag.InnerHtml.Append(i.ToString());
+
+                finishedTag.InnerHtml.AppendHtml(individualTag);
+
+            }
+
+            output.Content.AppendHtml(finishedTag.InnerHtml);
+        }
+    }
+}
